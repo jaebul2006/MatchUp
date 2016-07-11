0559cecc753366fdfb463a21016986a4e64f2a44
jdh <jaebul2006@naver.com>
2016-07-11 17:42:15 +0900

    사이트대문변경 및 url 관리추가

--- team/views.py
+++ team/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+﻿from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'team/post_list.html', {})
